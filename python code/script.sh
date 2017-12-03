681899c828ce9e02c0537315a87da1b6124bde8d
Matheus Telles <matheustwer@gmail.com>
2017-12-02 22:28:37 -0200

    Update script

--- python code/script.sh
+++ python code/script.sh
@@ -1,38 +1,42 @@
 BASE_PATH="../datasets/ptwiki-20170820"
 
-# Preprocssing
+# Preprocessing
 
-DOC_FORMAT="doc1kk"
-VOCAB_FORMAT="vocab30k"
+DOC_FORMAT="doc10k"
 
-ARTICLES=1000000
-TOKENS=30000
-DUMP_PATH="$BASE_PATH-pages-articles-parsed.json"
-FULL_SENTENCES_PATH="$BASE_PATH-sentences-$DOC_FORMAT.pickle"
-FULL_TOKENS_PATH="$BASE_PATH-tokens-$DOC_FORMAT.pickle"
-FULL_FREQUENCIES_PATH="$BASE_PATH-frequencies-$DOC_FORMAT.pickle"
-FILTERED_SENTENCES_PATH="$BASE_PATH-sentences-$DOC_FORMAT-$VOCAB_FORMAT.pickle"
-FILTERED_TOKENS_PATH="$BASE_PATH-tokens-$DOC_FORMAT-$VOCAB_FORMAT.pickle"
-FILTERED_FREQUENCIES_PATH="$BASE_PATH-frequencies-$DOC_FORMAT-$VOCAB_FORMAT.pickle"
+ARTICLES=10000
+CORPORA_PATH="$BASE_PATH-pages-articles-parsed.json"
+SENTENCES_PATH="$BASE_PATH-$DOC_FORMAT-sentences.pickle"
+TOKENS_PATH="$BASE_PATH-$DOC_FORMAT-tokens.pickle"
 STOPWORDS="../datasets/stopwords.txt"
 
-python3 preprocessing.py --n_articles $ARTICLES --n_tokens $TOKENS --wiki_dump $DUMP_PATH --stopwords $STOPWORDS --verbose True --full_sentences $FULL_SENTENCES_PATH  --full_tokens $FULL_TOKENS_PATH  --full_frequencies $FULL_FREQUENCIES_PATH  --filtered_sentences $FILTERED_SENTENCES_PATH  --filtered_tokens $FILTERED_TOKENS_PATH  --filtered_frequencies $FILTERED_FREQUENCIES_PATH
+python3 preprocessing.py preprocess --corpora $CORPORA_PATH --limit $ARTICLES --stopwords $STOPWORDS --sentences $SENTENCES_PATH --tokens $TOKENS_PATH --verbose True
+
+VOCAB_FORMAT="vocab30k"
+
+# Filter vocabulary
+
+TOKENS=30000
+FILTER_SENTENCES_PATH="$BASE_PATH-$DOC_FORMAT-$VOCAB_FORMAT-sentences.pickle"
+FILTER_TOKENS_PATH="$BASE_PATH-$DOC_FORMAT-$VOCAB_FORMAT-tokens.pickle"-$VOCAB_FORMAT
+
+python3 preprocessing.py filter --limit $TOKENS --raw_sentences $SENTENCES_PATH --sentences $FILTER_SENTENCES_PATH --tokens $FILTER_TOKENS_PATH --verbose True
 
 # Word2Vec training
 
-EMBEDDING_SIZE="embedding300"
+EMBEDDING_SIZE="embedding10"
 
-RANDOM_SIZE=100000000
-EMBEDDING=300
-ANNEAL_RATE=0.9
-ANNEAL_EVERY=100000
-PRINT_EVERY=10000
-SENTENCES_PATH="$BASE_PATH-sentences-$DOC_FORMAT-$VOCAB_FORMAT.pickle"
-TOKENS_PATH="$BASE_PATH-tokens-$DOC_FORMAT-$VOCAB_FORMAT.pickle"
-FREQUENCIES_PATH="$BASE_PATH-frequencies-$DOC_FORMAT-$VOCAB_FORMAT.pickle"
-W_IN_PATH="$BASE_PATH-win-$DOC_FORMAT-$VOCAB_FORMAT-$EMBEDDING_SIZE.pickle"
-W_OUT_PATH="$BASE_PATH-wout-$DOC_FORMAT-$VOCAB_FORMAT-$EMBEDDING_SIZE.pickle"
+RANDOM_SIZE=1000000
+EMBEDDING=10
+ANNEAL_RATE=0.8
+ANNEAL_EVERY=10000
+PRINT_EVERY=1000
+SENTENCES_PATH="$BASE_PATH-$DOC_FORMAT-$VOCAB_FORMAT-sentences.pickle"
+TOKENS_PATH="$BASE_PATH-$DOC_FORMAT-$VOCAB_FORMAT-tokens.pickle"
+FREQUENCIES_PATH="$BASE_PATH-$DOC_FORMAT-$VOCAB_FORMAT-frequencies.pickle"
+W_IN_PATH="$BASE_PATH-$DOC_FORMAT-$VOCAB_FORMAT-$EMBEDDING_SIZE-win.pickle"
+W_OUT_PATH="$BASE_PATH-$DOC_FORMAT-$VOCAB_FORMAT-$EMBEDDING_SIZE-wout.pickle"
 
-python3 word2vec.py --random $RANDOM_SIZE --embedding $EMBEDDING --anneal_rate $ANNEAL_RATE --anneal $ANNEAL_EVERY --print $PRINT_EVERY --sentences_path $SENTENCES_PATH  --tokens_path $TOKENS_PATH  --frequencies_path $FREQUENCIES_PATH --w_in_path $W_IN_PATH --w_out_path $W_OUT_PATH
+#python3 word2vec.py --random $RANDOM_SIZE --embedding $EMBEDDING --anneal_rate $ANNEAL_RATE --anneal $ANNEAL_EVERY --print $PRINT_EVERY --sentences_path $SENTENCES_PATH  --tokens_path $TOKENS_PATH  --frequencies_path $FREQUENCIES_PATH --w_in_path $W_IN_PATH --w_out_path $W_OUT_PATH
 
 
